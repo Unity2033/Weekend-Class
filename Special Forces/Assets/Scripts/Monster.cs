c446cbe71308b101840a0cca9454a3ecaf833bd9
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2023-11-24 21:47:39 +0900

    (2023.11.24) 몬스터 상태 패턴 추가

--- Special Forces/Assets/Scripts/Monster.cs
+++ Special Forces/Assets/Scripts/Monster.cs
@@ -3,17 +3,31 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
+public enum STATE
+{
+    WALK,
+    ATTACK,
+    DIE
+}
+
 public abstract class Monster : MonoBehaviour
 {
+    private float initSpeed;
     private Rigidbody2D rigidbody2D;
+    private protected Animator animator;
     private SpriteRenderer spriteRenderer;
 
+    [SerializeField] STATE state;
     [SerializeField] Vector2 direction;
-    [SerializeField] float speed = 100f;
     [SerializeField] Transform characterPosition;
+    [SerializeField] protected float speed = 100f;
+
 
     protected virtual void Start()
     {
+        initSpeed = speed;
+
+        animator = GetComponent<Animator>();
         rigidbody2D = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         characterPosition = GameObject.Find("Character").transform;
@@ -26,9 +40,15 @@ public abstract class Monster : MonoBehaviour
 
     protected virtual void FixedUpdate()
     {   
-        rigidbody2D.velocity = direction.normalized * speed * Time.fixedDeltaTime;
-
-        InvertImage();
+        switch(state)
+        {
+            case STATE.WALK : Move();
+                break;
+            case STATE.ATTACK : Attack();
+                break;
+            case STATE.DIE : Death(); 
+                break;
+        }
     }
 
     public void InvertImage()
@@ -36,5 +56,34 @@ public abstract class Monster : MonoBehaviour
         spriteRenderer.flipX = (direction.x < 0) ? true : false;
     }
 
+    protected void Move()
+    {
+        speed = initSpeed;
+
+        animator.SetBool("Attack", false);
+
+        rigidbody2D.velocity = direction.normalized * speed * Time.fixedDeltaTime;
+
+        InvertImage();
+    }
+
     protected abstract void Attack();
+
+    protected abstract void Death();
+
+    protected void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Character"))
+        {
+            state = STATE.ATTACK;
+        }
+    }
+
+    protected void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Character"))
+        {
+            state = STATE.WALK;
+        }
+    }
 }
