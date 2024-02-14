bd97334d349084fa91b466c09fe2c209b0a335eb
Unity2033 <kileab@naver.com>
2024-02-14 19:44:40 +0900

    (2024.2.14) 룸 시스템

---
+++ Photon Network/Assets/Scripts/Information.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using TMPro;
+
+public class Information : MonoBehaviourPunCallbacks
+{
+    [SerializeField] string roomName;
+    public TextMeshProUGUI roomInformation;
+
+    public void ConnectRoom()
+    {
+        PhotonNetwork.JoinRoom(roomName);
+    }
+
+    public void RoomData(string name, int currentStaff, int maxStaff)
+    {
+        roomName = name;
+
+        roomInformation.fontSize = 50;
+        roomInformation.text = name + " ( " + currentStaff + " / " + maxStaff + " ) "; 
+    }
+}
